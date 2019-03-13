ad0b2beb6cbae599032b42a7df4cdd771ccac6a5
Maximo Bautista <max.bautista97@gmail.com>
2019-03-13 15:38:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+document.getElementById('main').remove();
+const newHeader = document.createElement('H1');
+newHeader.id = "victory";
+newHeader.innerText = 'YOUR-NAME is the champion';
